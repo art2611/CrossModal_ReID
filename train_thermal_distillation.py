1102876e84d83a4368c3de697df67fc7d12ae276
art2611 <art261197@hotmail.fr>
2020-11-15 22:41:02 +0100

    k

--- train_thermal_distillation.py
+++ train_thermal_distillation.py
@@ -189,7 +189,9 @@ def multi_process() :
 
             # feat is the feature vector out of
             # Out is the last output
-            feat1, out1, = net_visible(visible_input)  # Call the visible branch only
+            with torch.no_grad():
+                net_visible.train()
+                feat1, out1, = net_visible(visible_input)  # Call the visible branch only
 
             feat2, out2, = net_thermal(thermal_input)
 
