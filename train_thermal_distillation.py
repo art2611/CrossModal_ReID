d2fb0751b8a0cb78d56cd50ca4b61d60f7629b14
art2611 <art261197@hotmail.fr>
2020-11-15 22:32:09 +0100

    k

--- train_thermal_distillation.py
+++ train_thermal_distillation.py
@@ -188,9 +188,9 @@ def multi_process() :
             # feat is the feature vector out of
             # Out is the last output
             feat1, out1, = net_visible(visible_input)  # Call the visible branch only
-            sys.exit()
-            feat2, out2, = net_visible(thermal_input)
 
+            feat2, out2, = net_visible(visible_input)
+            sys.exit()
 
             loss_MSE = criterion_MSE(out1, out2)
 
