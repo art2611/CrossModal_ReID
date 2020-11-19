166a23990091914d92328261da84d02fc3bfc13c
art2611 <art261197@hotmail.fr>
2020-11-19 11:22:22 +0100

    k

--- test_distillation.py
+++ test_distillation.py
@@ -153,9 +153,9 @@ def multi_process():
         distmat_pool = np.matmul(gall_feat_pool, np.transpose(query_feat_pool))
         # print(-distmat_pool[0])
         # print(np.argsort(-distmat_pool, axis = 1)[0])
-        print(type(gall_feat_pool.shape[0]))  # Number of gallery images
+        print(gall_feat_pool.shape[0])  # Number of gallery images
         print(query_feat_pool.shape[0])  # Number of query images
-        distance = np.zeros(gall_feat_pool.shape[0], 84)
+        distance = np.zeros((gall_feat_pool.shape[0], query_feat_pool.shape[0]))
 
         for i in range(gall_feat_pool.shape[0]):
             for j in range(gall_feat_pool.shape[0]):
