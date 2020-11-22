7e2271fc2299935af3bc31faa3ce004be15eef6a
art2611 <art261197@hotmail.fr>
2020-11-22 15:48:08 +0100

    k

--- test_single.py
+++ test_single.py
@@ -132,8 +132,8 @@ def multi_process() :
                 query_img, query_label, gall_img, gall_label = \
                 process2_test_sysu(data_path, modal=args.train, ir_img =ir_img , ir_id=ir_id, ir_pos = ir_pos)
 
-        print(len(query_label))
-        print(len(gall_label))
+        print(len(np.unique(query_label)))
+        print(len(np.unique(gall_label)))
         sys.exit()
 
         gallset = TestData(gall_img, gall_label, transform=transform_test, img_size=(img_w, img_h))
