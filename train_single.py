160eb4a44525e6ddd0b33b0182cee2a71b3e648d
art2611 <art261197@hotmail.fr>
2020-11-22 19:24:48 +0100

    k

--- train_single.py
+++ train_single.py
@@ -29,7 +29,7 @@ def multi_process() :
 
     # device = 'cpu'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    writer = SummaryWriter("runs/NewVisible3")
+    writer = SummaryWriter("runs/NewVisible4")
 
     # Init variables :
     img_w = 144
