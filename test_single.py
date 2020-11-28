d2c8adb7001f395506fe13bea8112c2506cbb187
art2611 <art261197@hotmail.fr>
2020-11-28 16:01:59 +0100

    k

--- test_single.py
+++ test_single.py
@@ -122,90 +122,163 @@ def multi_process() :
     else :
         sys.exit("Saved model not found")
     # Building test set and data loaders
+    if args.dataset == "regdb" :
+        for trial in range(1, 11):
+            if args.dataset == "regdb" :
+                query_img, query_label, gall_img, gall_label = process_test_regdb(data_path, modal=args.train, split=True, trial = trial)
+            elif args.dataset == "sysu" :
+                ir_img, ir_id, vis_img, vis_id = process_test_sysu(data_path)
+                vis_pos, ir_pos  = GenIdx(vis_id, ir_id)
+                if args.train == "visible" :
+                    query_img, query_label, gall_img, gall_label = \
+                    process2_test_sysu(data_path, modal=args.train, vis_img=vis_img, vis_id=vis_id, vis_pos=vis_pos )
+                if args.train == "thermal" :
+                    query_img, query_label, gall_img, gall_label = \
+                    process2_test_sysu(data_path, modal=args.train, ir_img =ir_img , ir_id=ir_id, ir_pos = ir_pos)
 
-    for trial in range(1, 11):
-        if args.dataset == "regdb" :
-            query_img, query_label, gall_img, gall_label = process_test_regdb(data_path, modal=args.train, split=True, trial = trial)
-        elif args.dataset == "sysu" :
-            ir_img, ir_id, vis_img, vis_id = process_test_sysu(data_path)
-            vis_pos, ir_pos  = GenIdx(vis_id, ir_id)
-            if args.train == "visible" :
-                query_img, query_label, gall_img, gall_label = \
-                process2_test_sysu(data_path, modal=args.train, vis_img=vis_img, vis_id=vis_id, vis_pos=vis_pos )
-            if args.train == "thermal" :
-                query_img, query_label, gall_img, gall_label = \
-                process2_test_sysu(data_path, modal=args.train, ir_img =ir_img , ir_id=ir_id, ir_pos = ir_pos)
+            gallset = TestData(gall_img, gall_label, transform=transform_test, img_size=(img_w, img_h))
+            gall_loader = torch.utils.data.DataLoader(gallset, batch_size=test_batch_size, shuffle=False, num_workers=workers)
 
-        gallset = TestData(gall_img, gall_label, transform=transform_test, img_size=(img_w, img_h))
-        gall_loader = torch.utils.data.DataLoader(gallset, batch_size=test_batch_size, shuffle=False, num_workers=workers)
+            nquery = len(query_label)
+            ngall = len(gall_label)
+
+            queryset = TestData(query_img, query_label, transform=transform_test, img_size=(img_w, img_h))
+            query_loader = torch.utils.data.DataLoader(queryset, batch_size=test_batch_size, shuffle=False, num_workers=4)
+
+            print('Data Loading Time:\t {:.3f}'.format(time.time() - end))
+
+            ############################## DISPLAY QUERY AND GALLERY
+            # for i in range(6):
+            #      plt.subplot(2, 3, i + 1)
+            #      if i < 3 :
+            #         plt.title("Gallery")
+            #         plt.imshow(np.array(gallset.test_image[i*10]))
+            #      else :
+            #         plt.title("Query")
+            #         plt.imshow(np.array(queryset.test_image[i*10]))
+            #      # plt.imshow(final_train_data[i], cmap='gray')
+            # plt.show()
+
+            query_feat_pool, query_feat_fc = extract_query_feat(query_loader, nquery = nquery, net = net)
+            gall_feat_pool,  gall_feat_fc = extract_gall_feat(gall_loader, ngall = ngall, net = net)
+
+            # if True = thermal to visible, else, the reverse
+            if True :
+                # pool5 feature
+                distmat_pool = np.matmul(gall_feat_pool, np.transpose(query_feat_pool))
+                if args.dataset=="regdb":
+                    cmc_pool, mAP_pool, mINP_pool = eval_regdb(-distmat_pool, gall_label, query_label)
+                if args.dataset=="sysu":
+                    cmc_pool, mAP_pool, mINP_pool = eval_sysu(-distmat_pool, gall_label, query_label)
+
+                # fc feature
+                distmat = np.matmul(gall_feat_fc , np.transpose(query_feat_fc))
+                cmc, mAP, mINP = eval_regdb(-distmat,gall_label,  query_label )
+            else:
+                # pool5 feature
+                distmat_pool = np.matmul(query_feat_pool, np.transpose(gall_feat_pool))
+                cmc_pool, mAP_pool, mINP_pool = eval_regdb(-distmat_pool, query_label, gall_label)
+
+                # fc feature
+                distmat = np.matmul(query_feat_fc, np.transpose(gall_feat_fc))
+                cmc, mAP, mINP = eval_regdb(-distmat, query_label, gall_label)
+
+            if trial == 1 :
+                all_cmc = cmc
+                all_mAP = mAP
+                all_mINP = mINP
+                all_cmc_pool = cmc_pool
+                all_mAP_pool = mAP_pool
+                all_mINP_pool = mINP_pool
+            else:
+                all_cmc = all_cmc + cmc
+                all_mAP = all_mAP + mAP
+                all_mINP = all_mINP + mINP
+                all_cmc_pool = all_cmc_pool + cmc_pool
+                all_mAP_pool = all_mAP_pool + mAP_pool
+                all_mINP_pool = all_mINP_pool + mINP_pool
+
+            print('Test Trial: {}'.format(trial))
+            print(
+                'FC:     Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
+                    cmc[0], cmc[4], cmc[9], cmc[19], mAP, mINP))
+            print(
+                'POOL:   Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
+                    cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19], mAP_pool, mINP_pool))
+
+    if dargs.dataset == 'sysu':
+
+        print('==> Resuming from checkpoint..')
+        model_path = checkpoint_path + suffix + '_best.t'
+        # model_path = checkpoint_path + 'regdb_awg_p4_n8_lr_0.1_seed_0_trial_{}_best.t'.format(test_trial)
+        if os.path.isfile(model_path):
+            print('==> loading checkpoint')
+            checkpoint = torch.load(model_path)
+            net = Network(class_num=nclass)
+            net = net.to(device)
+            net.load_state_dict(checkpoint['net'])
+        else :
+            print("Saved model not loaded, care")
+            net = Network(class_num = nclass).to(device)
+
+        # testing set
+        query_img, query_label, query_cam = process_query_sysu(data_path, mode="all")
+        gall_img, gall_label, gall_cam = process_gallery_sysu(data_path, mode="all", trial=0)
 
         nquery = len(query_label)
         ngall = len(gall_label)
+        print("Dataset statistics:")
+        print("  ------------------------------")
+        print("  subset   | # ids | # images")
+        print("  ------------------------------")
+        print("  query    | {:5d} | {:8d}".format(len(np.unique(query_label)), nquery))
+        print("  gallery  | {:5d} | {:8d}".format(len(np.unique(gall_label)), ngall))
+        print("  ------------------------------")
 
         queryset = TestData(query_img, query_label, transform=transform_test, img_size=(img_w, img_h))
-        query_loader = torch.utils.data.DataLoader(queryset, batch_size=test_batch_size, shuffle=False, num_workers=4)
-
+        query_loader = data.DataLoader(queryset, batch_size=test_batch_size, shuffle=False, num_workers=4)
         print('Data Loading Time:\t {:.3f}'.format(time.time() - end))
 
-        ############################## DISPLAY QUERY AND GALLERY
-        # for i in range(6):
-        #      plt.subplot(2, 3, i + 1)
-        #      if i < 3 :
-        #         plt.title("Gallery")
-        #         plt.imshow(np.array(gallset.test_image[i*10]))
-        #      else :
-        #         plt.title("Query")
-        #         plt.imshow(np.array(queryset.test_image[i*10]))
-        #      # plt.imshow(final_train_data[i], cmap='gray')
-        # plt.show()
+        query_feat_pool, query_feat_fc = extract_query_feat(query_loader,nquery = nquery, net = net)
 
-        query_feat_pool, query_feat_fc = extract_query_feat(query_loader, nquery = nquery, net = net)
-        gall_feat_pool,  gall_feat_fc = extract_gall_feat(gall_loader, ngall = ngall, net = net)
+        for trial in range(10):
 
-        # if True = thermal to visible, else, the reverse
-        if True :
-            # pool5 feature
-            distmat_pool = np.matmul(gall_feat_pool, np.transpose(query_feat_pool))
-            if args.dataset=="regdb":
-                cmc_pool, mAP_pool, mINP_pool = eval_regdb(-distmat_pool, gall_label, query_label)
-            if args.dataset=="sysu":
-                cmc_pool, mAP_pool, mINP_pool = eval_sysu(-distmat_pool, gall_label, query_label)
+            gall_img, gall_label, gall_cam = process_gallery_sysu(data_path, mode="all",  trial=trial)
 
-            # fc feature
-            distmat = np.matmul(gall_feat_fc , np.transpose(query_feat_fc))
-            cmc, mAP, mINP = eval_regdb(-distmat,gall_label,  query_label )
-        else:
+            trial_gallset = TestData(gall_img, gall_label, transform=transform_test, img_size=(img_w, img_h))
+            trial_gall_loader = data.DataLoader(trial_gallset, batch_size=test_batch_size, shuffle=False, num_workers=4)
+
+            gall_feat_pool, gall_feat_fc = extract_gall_feat(trial_gall_loader,ngall = ngall, net = net)
+
             # pool5 feature
             distmat_pool = np.matmul(query_feat_pool, np.transpose(gall_feat_pool))
-            cmc_pool, mAP_pool, mINP_pool = eval_regdb(-distmat_pool, query_label, gall_label)
+            cmc_pool, mAP_pool, mINP_pool = eval_sysu(-distmat_pool, query_label, gall_label, query_cam, gall_cam)
 
             # fc feature
             distmat = np.matmul(query_feat_fc, np.transpose(gall_feat_fc))
-            cmc, mAP, mINP = eval_regdb(-distmat, query_label, gall_label)
+            cmc, mAP, mINP = eval_sysu(-distmat, query_label, gall_label, query_cam, gall_cam)
+            if trial == 0:
+                all_cmc = cmc
+                all_mAP = mAP
+                all_mINP = mINP
+                all_cmc_pool = cmc_pool
+                all_mAP_pool = mAP_pool
+                all_mINP_pool = mINP_pool
+            else:
+                all_cmc = all_cmc + cmc
+                all_mAP = all_mAP + mAP
+                all_mINP = all_mINP + mINP
+                all_cmc_pool = all_cmc_pool + cmc_pool
+                all_mAP_pool = all_mAP_pool + mAP_pool
+                all_mINP_pool = all_mINP_pool + mINP_pool
 
-        if trial == 1 :
-            all_cmc = cmc
-            all_mAP = mAP
-            all_mINP = mINP
-            all_cmc_pool = cmc_pool
-            all_mAP_pool = mAP_pool
-            all_mINP_pool = mINP_pool
-        else:
-            all_cmc = all_cmc + cmc
-            all_mAP = all_mAP + mAP
-            all_mINP = all_mINP + mINP
-            all_cmc_pool = all_cmc_pool + cmc_pool
-            all_mAP_pool = all_mAP_pool + mAP_pool
-            all_mINP_pool = all_mINP_pool + mINP_pool
-
-        print('Test Trial: {}'.format(trial))
-        print(
-            'FC:     Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
-                cmc[0], cmc[4], cmc[9], cmc[19], mAP, mINP))
-        print(
-            'POOL:   Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
-                cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19], mAP_pool, mINP_pool))
-
+            print('Test Trial: {}'.format(trial))
+            print(
+                'FC:   Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
+                    cmc[0], cmc[4], cmc[9], cmc[19], mAP, mINP))
+            print(
+                'POOL: Rank-1: {:.2%} | Rank-5: {:.2%} | Rank-10: {:.2%}| Rank-20: {:.2%}| mAP: {:.2%}| mINP: {:.2%}'.format(
+                    cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19], mAP_pool, mINP_pool))
     cmc = all_cmc / 10
     mAP = all_mAP / 10
     mINP = all_mINP / 10
